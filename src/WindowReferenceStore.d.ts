4c7d7a96e6a6c1924e4c2ad0ada46b30fad0cc54
Wezz Balk <wezz@wezz.se>
2022-03-14 11:06:27 +0700

    Attempt to add typescript definitions

---
+++ src/WindowReferenceStore.d.ts
@@ -0,0 +1,11 @@
+﻿declare class WindowReferenceStore {
+  private storeNamespace;
+  private storeName;
+  private root;
+  constructor(storeName?: string, namespace?: string, parent?: null);
+  private registerGlobalReferences;
+  has(key: any): boolean;
+  get(key: any): any;
+  set(key: string, ref: any, override?: boolean): boolean;
+  remove(key: any): boolean;
+}
