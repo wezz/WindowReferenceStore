b4e35c3ab5b9e76f9ed08ccbce82b949e52c22a0
Wezz Balk <wezz@wezz.se>
2022-03-14 04:07:14 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @wezz/window-reference-store v0.0.3
+ *   @wezz/window-reference-store v0.0.4
  *   https://github.com/wezz/WindowReferenceStore
  *
  *   Copyright (c) Wezz Balk (https://github.com/wezz)
