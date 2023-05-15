934b12e7e783704ef16fc8be374bab8ceab25d84
Wezz Balk <wezz@wezz.se>
2023-05-15 10:21:23 +0200

    Update definition

--- dist/WindowReferenceStore.d.ts
+++ dist/WindowReferenceStore.d.ts
@@ -4,11 +4,12 @@ export default class WindowReferenceStore {
     private root;
     constructor(storeName?: string, namespace?: string, parent?: any);
     private registerGlobalReferences;
-    has(key: any): boolean;
-    get(key: any): any;
+    has(key: string): boolean;
+    get(key: string): any;
     set(key: string, ref: any, override?: boolean): boolean;
-    remove(key: any): boolean;
+    remove(key: string): boolean;
 }
+
 export interface WindowReferenceStoreClass {
     constructor(storeName?: string, namespace?: string, parent?: null): any;
     has(key: any): boolean;
